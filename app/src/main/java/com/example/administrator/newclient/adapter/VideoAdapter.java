9c61683af3486d61369ae8312ca440361f566e89
lhy <lhy1234567891011@163.com>
2017-06-29 21:27:12 +0800

    视频播放

---
+++ app/src/main/java/com/example/administrator/newclient/adapter/VideoAdapter.java
@@ -0,0 +1,113 @@
+package com.example.administrator.newclient.adapter;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.administrator.newclient.R;
+import com.example.administrator.newclient.bean.VideoEntity;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
+
+
+/**
+ * 视频列表适配器
+ *
+ * @author WJQ
+ */
+public class VideoAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+
+    /**
+     * 列表显示的数据
+     */
+    private List<VideoEntity.ResultBean> listDatas;
+
+    // 构造方法
+    public VideoAdapter(Context context, List<VideoEntity.ResultBean> listDatas) {
+        this.context = context;
+        this.listDatas = listDatas;
+    }
+
+    // 创建列表项, 返回ViewHolder
+    // ViewHolder: 避免每次都findViewById查找子控件，
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 创建列表项
+        View itemView = LayoutInflater.from(context).inflate(
+                R.layout.item_video, parent, false);
+
+        // 创建ViewHolder，并返回
+        return new MyViewHolder(itemView);
+    }
+
+    // 刷新列表项中的子控件
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, final int position) {
+        MyViewHolder holder = (MyViewHolder) viewHolder;
+
+        // 列表项对应的数据
+        VideoEntity.ResultBean video = listDatas.get(position);
+
+        // 显示列表项中的子控件
+
+        // 显示视频标题
+        holder.tvVideoTitle.setText(video.getTitle());
+        // 显示视频播放时长
+        String duration = video.getLength() / 60 + "分" + video.getLength() % 60 + "秒";
+        holder.tvVideoDuration.setText(duration);
+        // 显示视频预览图片
+        Picasso.with(context).load(video.getCover()).into(holder.ivVideoImage);
+
+        // 显示列表项中的视频
+        // 参数1: 要播放视频的url
+        // 参数2: 视频标题，会显示JCVideoPlayerStandard控件上
+        holder.videoPlayer.setUp(video.getMp4_url(), /*video.getTitle()*/"");
+
+        // 设置列表项点击事件
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // 有多少个列表项
+    @Override
+    public int getItemCount() {
+        return (listDatas == null) ? 0 : listDatas.size();
+    }
+
+    private class MyViewHolder extends RecyclerView.ViewHolder {
+
+        private ImageView ivVideoImage;
+        private TextView tvVideoTitle;
+        private TextView tvVideoDuration;
+        private TextView tvPlayCount;
+        private JCVideoPlayerStandard videoPlayer;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            ivVideoImage = (ImageView) itemView.findViewById(R.id.iv_video_image);
+            tvVideoTitle = (TextView) itemView.findViewById(R.id.tv_video_title);
+            tvVideoDuration = (TextView) itemView.findViewById(R.id.tv_video_duration);
+            tvPlayCount = (TextView) itemView.findViewById(R.id.tv_play_count);
+            videoPlayer = (JCVideoPlayerStandard) itemView.findViewById(R.id.videoplayer_standard);
+        }
+    }
+}
